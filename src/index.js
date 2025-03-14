027eeddce723613c591efa6ec9018e8416c9239c
estherbg85 <estherbonillagirol85@gmail.com>
2025-03-14 22:14:23 +0100

    endpoint post

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ async function getConnection() {
     password: process.env["MYSQL_PASS"],
     database: process.env["MYSQL_SCHEMA"],
   };
-
+  console.log(connectionData);
   const connection = await mysql.createConnection(connectionData);
   await connection.connect();
 
@@ -28,23 +28,59 @@ app.use(express.json());
 
 //Arrancamos el servidor
 
-const port = 3000;
+const port = process.env["PORT"] || 3000;
+
 app.listen(port, () => {
-  console.log(`Example app listening on port <http://localhost:${port}>`);
+  console.log(`App listening at <http://localhost:${port}>`);
 });
 
 //Endpoints
 
-app.get("/cuentos", async (req, res) => {
+app.get("/api/books", async (req, res) => {
   const conn = await getConnection();
 
-  const [results] = await conn.query(`SELECT * FROM cuentos;`);
+  const [results] = await conn.query(`SELECT * FROM books;`);
 
   await conn.end();
+
   const numOfElements = results.length;
 
   res.json({
     info: { count: numOfElements },
     results: results,
+  });
+});
+
+app.post("/api/books", async (req, res) => {
+  if (req.body.name === "") {
+    return res.status(400).json({
+      success: false,
+      error: "the name is incorrect",
+    });
+  }
+
+  const conn = await getConnection();
+
+  const [result] = await conn.execute(
+    `INSERT INTO books
+    (name, description, ages_id_age, publishters_id_publishter)
+    VALUES (?,?,?,?);`,
+    [
+      req.body.name,
+      req.body.description,
+      req.body.ages_id_age,
+      req.body.publishters_id_publishter,
+    ]
+  );
+
+  await conn.end();
+
+  res.json({
+    success: true,
+    id: result.insertId,
+    bookObj: {
+      ...req.body,
+      id: result.insertId,
+    },
   });
 });
